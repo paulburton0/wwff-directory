738caead4d7c48e6c7de6dded9f488a5789a4857
Paul Burton <paulburton0@gmail.com>
2017-10-06 18:00:43 -0400

    Initial commit

---
+++ routes/index.js
@@ -0,0 +1,27 @@
+var express = require('express');
+var router = express.Router();
+var MongoClient = require('mongodb').MongoClient;
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+	MongoClient.connect('mongodb://localhost:27017/wwff', function(err, db) {
+
+		// Create a collection we want to drop later
+		var collection = db.collection('directory');
+
+		// Perform a simple find and return all the documents
+		collection.find().toArray(function(err, docs) {
+            db.close();
+            docs.sort(function(a,b){
+                if (a.Ref < b.Ref)
+                    return -1;
+                if (a.Ref > b.Ref)
+                    return 1;
+                return 0;
+            });
+            res.render('index', { docs, title: 'Directory of KFF Parks' });
+		});
+	});
+});
+
+module.exports = router;
